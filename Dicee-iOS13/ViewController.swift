51de98dbfbf2026c45df0ec5463082849ba0ce99
SanTMK <santihh2001@gmail.com>
2022-02-12 15:41:26 -0500

    Finished editing the image views for my game as well as Angela's game

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var diceLeft: UIImageView!
+    
+    @IBOutlet weak var diceRight: UIImageView!
+    
+    let rolls = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func diceRoll(_ sender: Any) {
+        
+        diceLeft.image = rolls[Int.random(in: 0...5)]
+        diceRight.image = rolls[Int.random(in: 0...5)]
+    }
+    
 }
 
